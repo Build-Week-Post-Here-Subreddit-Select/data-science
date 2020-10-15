7d5a6bf7374f32420abade182288e375c6a1410f
Reece323 <codyreece323@gmail.com>
2020-10-15 00:18:52 -0500

    reformat files to attempt 2nd deploy to remote

---
+++ app_api/app.py
@@ -0,0 +1,22 @@
+from flask import Flask #, request, jsonify
+#from pathlib import Path
+from flask_cors import CORS #, cross_origin
+import json
+
+def create_app():
+    '''Create and configure an instance of the Flask application'''
+
+    app = Flask(__name__)
+    CORS(app)
+
+    @app.route('/')
+    def root():
+        return 'Generic Return'
+    return app
+
+    @app.route('/predict', methods=['POST'])
+    def predict_sub():
+
+        return(json.dumps({'input':'test', 'predict':'r/AdviceAnimals'}))
+
+    return app
