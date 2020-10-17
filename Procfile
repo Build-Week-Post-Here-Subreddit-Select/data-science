2e24af9fa03dfc28e20d3ab18dda03b978827587
Reece323 <codyreece323@gmail.com>
2020-10-17 13:20:06 -0500

    Deploying fastapi version of app to Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app_api:APP -t 120
+web: uvicorn --host 0.0.0.0 --port $PORT app.main:app
--- app_api/__init__.py
+++
@@ -1,4 +0,0 @@
-'''Entry point to the Flask application'''
-from .app import create_app
-
-APP = create_app()
